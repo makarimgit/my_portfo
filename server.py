01155bb87f358185bd2844ddb15dfbe0de7d9f7e
Makarim <makarim@pop-os.localdomain>
2020-09-19 20:17:19 +0530

    changes in my_portfo

--- server.py
+++ server.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, url_for, request, redirect
 import csv
+
+
 app = Flask(__name__)
 print(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return render_template('index_dup.html')
+    return render_template('index.html')
 
 
 @app.route('/<string:page_name>')
